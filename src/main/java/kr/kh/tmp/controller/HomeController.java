e0618c28a0fdbe1b02d341034e9e2b9780f1e56d
Soon17 <acejsh77@naver.com>
2025-03-24 14:37:59 +0900

    회원가입 실패 시 아이디 남김 처리

--- src/main/java/kr/kh/tmp/controller/HomeController.java
+++ src/main/java/kr/kh/tmp/controller/HomeController.java
@@ -25,7 +25,8 @@ public class HomeController {
 	
 //	화면 띄울 때 사용됨
 	@GetMapping("/signup")
-	public String signup() {
+	public String signup(Model model, String id) {
+		model.addAttribute("id", id);
 		return "/member/signup";
 	}
 	
@@ -36,7 +37,7 @@ public class HomeController {
 			model.addAttribute("url", "/");			//성공하면 home
 			model.addAttribute("msg", "회원가입에 성공했습니다.");
 		} else {
-			model.addAttribute("url", "/signup");	//실패하면 화면 유지
+			model.addAttribute("url", "/signup?id=" + member.getMe_id());	//실패하면 화면 유지
 			model.addAttribute("msg", "회원가입에 실패했습니다.");
 		}
 		return "message";
