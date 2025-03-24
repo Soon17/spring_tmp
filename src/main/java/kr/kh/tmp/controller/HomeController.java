18e3e2f084870a89e6361a8b9bb62b96e3fb7f41
Soon17 <acejsh77@naver.com>
2025-03-24 14:25:24 +0900

    회원가입 구현 완료

--- src/main/java/kr/kh/tmp/controller/HomeController.java
+++ src/main/java/kr/kh/tmp/controller/HomeController.java
@@ -2,6 +2,7 @@ package kr.kh.tmp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,18 +23,22 @@ public class HomeController {
 		return "home";
 	}
 	
+//	화면 띄울 때 사용됨
 	@GetMapping("/signup")
 	public String signup() {
 		return "/member/signup";
 	}
 	
+//	화면 값을 받아올 때 사용됨
 	@PostMapping("/signup")
-	public String signupPost(MemberVO member) {
+	public String signupPost(Model model, MemberVO member) {
 		if(memberService.signup(member)) {
-			
+			model.addAttribute("url", "/");			//성공하면 home
+			model.addAttribute("msg", "회원가입에 성공했습니다.");
 		} else {
-			
+			model.addAttribute("url", "/signup");	//실패하면 화면 유지
+			model.addAttribute("msg", "회원가입에 실패했습니다.");
 		}
-		return "/member/signup";
+		return "message";
 	}
 }
