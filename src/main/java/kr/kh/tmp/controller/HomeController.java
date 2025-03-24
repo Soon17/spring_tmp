201302ed82ba99145a45cba44ac3a95ede17dfcd
Soon17 <acejsh77@naver.com>
2025-03-24 11:06:28 +0900

    soon2 와 병합

--- src/main/java/kr/kh/tmp/controller/HomeController.java
+++ src/main/java/kr/kh/tmp/controller/HomeController.java
@@ -4,12 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class HomeController {
 	
+	@Autowired
+	private MemberService memberService;
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home() {
 		
